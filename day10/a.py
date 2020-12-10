d12a0950e7c5517d6a1cc47039411ce629d9d7d5
“jl3663” <jl3663@cornell.edu>
2020-12-10 00:43:51 -0500

    day 10 DP 0.0

---
+++ day10/a.py
@@ -0,0 +1,51 @@
+import math
+
+
+def read_input():
+    with open("../inputs/day10.txt") as f:
+        numbers = [int(line.strip()) for line in f]
+    return numbers
+
+
+def find_joltage(numbers):
+    difference = {1: 0, 2: 0, 3: 0}
+    joltage = 0
+    maxj = max(numbers)
+    while joltage < maxj:
+        passa = []
+        for j in [1, 2, 3]:
+            if joltage + j in numbers:
+                passa.append(j)
+        n = min(passa)
+        difference[n] += 1
+        joltage += n
+    print(difference[1] * (difference[3] + 1))
+    return difference
+
+
+def find_ways(numbers):
+    numbers.append(0)
+    ways = [0] * (max(numbers) + 1)
+    ways[0] = 1
+    if 1 in numbers:
+        ways[1] = 1
+    if 2 in numbers:
+        ways[2] = ways[1] + 1
+    numbers = sorted(numbers)
+    for i in numbers:
+        if i == 1 or i == 2:
+            continue
+        for j in [1, 2, 3]:
+            if i - j in numbers:
+                ways[i] += ways[i - j]
+    print(ways[-1])
+
+
+def main():
+    numbers = read_input()
+    find_joltage(numbers)
+    find_ways(numbers)
+
+
+if __name__ == "__main__":
+    main()
